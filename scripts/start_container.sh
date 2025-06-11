f48ec92393a979c76de0c7079e63df62867ccb26
SSankalp470 <01saiaws@gmail.com>
2025-06-11 13:10:53 +0000

    CICD 01

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ssankalp470/docker-cicd-webapp
+docker pull ssankalp470/buildemo
 
 # Run the Docker image as a container
-docker run -d -p 8080:8080 ssankalp470/docker-cicd-webapp
+docker run -d -p 80:80 ssankalp470/builddemo
