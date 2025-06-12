a613dab70bd414c136260e47e6ac408f66532055
SSankalp470 <01saiaws@gmail.com>
2025-06-12 07:37:42 +0000

    commit cicd

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM amazonlinux
 RUN yum install httpd -y
 
 COPY index.html /var/www/html
+
+ENTRYPOINT ["/usr/sbin/httpd", "-D", "FOREGROUND"]
--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
-
-# Pull the Docker image from Docker Hub
 docker pull ssankalp470/builddemo:v1
-
-# Run the Docker image as a container
 docker run -d -p 80:80 ssankalp470/builddemo:v1
