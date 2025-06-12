febc2df38a71c64b7ef5e166b051913599d1c9b0
SSankalp470 <01saiaws@gmail.com>
2025-06-12 06:52:57 +0000

    docker

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ssankalp470/buildemo
+docker pull ssankalp470/buildemo:v1
 
 # Run the Docker image as a container
-docker run -d -p 80:80 ssankalp470/builddemo
+docker run -d -p 80:80 ssankalp470/builddemo:v1
