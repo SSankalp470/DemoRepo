6fde28e3e18985aadf21b6e5b6a9af9534a2e399
SSankalp470 <01saiaws@gmail.com>
2025-06-12 07:15:36 +0000

    docker fix

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ssankalp470/buildemo:v1
+docker pull ssankalp470/builddemo:v1
 
 # Run the Docker image as a container
 docker run -d -p 80:80 ssankalp470/builddemo:v1
